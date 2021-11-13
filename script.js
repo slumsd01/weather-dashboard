bd3a807ceec5f9138f518e8a4a07fb5b12c36027
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-11-12 23:44:43 -0500

    get weather information

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-let APIkey = "c08f8f49cb7a0258a03859cd344d9035";
+let apikey = "c08f8f49cb7a0258a03859cd344d9035";
+
+$("#searchbutton").on("click", function(event){
+    let cityname = $("#citysearch").val(); 
+    console.log(cityname); // console log search input
+
+
+    function getweather(cityname) {
+        let queryurl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + apikey;
+        $.get(queryurl, function(data) {
+            console.log(data);
+        })
+    }
+
+    getweather(cityname); // get weather data for city
+
+
+
+
+
+    
+
+    //display current conditions
+
+    //display forcast
+
+    //display recent searches
+
+
+
+
+
+
+
+    event.preventDefault();
+});
