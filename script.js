31fe1a97ae083966ab2a8f4140461d2af5e1f2eb
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-11-13 00:29:16 -0500

    display city name & date

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 let apikey = "c08f8f49cb7a0258a03859cd344d9035";
 
+let cityEl = $("#city");
+let dateEl = $("#date");
+let currenttempEl = $("#current-temp");
+let currentwindEl = $("#current-wind");
+let currenthumidityEl = $("#current-humidity");
+let currentuvindexEl = $("#current-uv-index");
+
+let day1dateEl = $("#day1-date");
+let day1tempEl = $("#day1-temp");
+let day1windEl = $("#day1-wind");
+let day1humidityEl = $("#day1-humidity");
+
+//get today's date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+
 $("#searchbutton").on("click", function(event){
     let cityname = $("#citysearch").val(); 
     console.log(cityname); // console log search input
 
-
-    function getweather(cityname) {
+    //get current weather data for city
+    function getcurrentweather(cityname) {
         let queryurl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + apikey;
         $.get(queryurl, function(data) {
             console.log(data);
+
+            //display current conditions
+            console.log(data.name); //city name
+            console.log(today); //today's date
+            cityEl.text(data.name + " (" + today +")");
+
+
+
+
+
+
         })
     }
-
-    getweather(cityname); // get weather data for city
-
-
-
-
-
-    
-
-    //display current conditions
-
-    //display forcast
-
-    //display recent searches
-
-
-
-
-
-
-
+    getcurrentweather(cityname); 
     event.preventDefault();
 });
